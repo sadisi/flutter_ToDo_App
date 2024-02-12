df46befa03053f4a867f35e298ba944ee5a5dc7e
sadisi <sadisi.rajapaksha218346@gmail.com>
2024-02-12 14:30:57 +0530

    add prifile logo

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -9,13 +9,20 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: tdBGColor,
-        title: Row(children: [
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
           Icon(Icons.menu,
           color: tdBlack,
           size: 30,
           ),
           Container(
             height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/avatar.jpg'),
+            ),
           )
         ],),
       ),
