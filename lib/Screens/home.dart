55583f77cfb77357abade825959de189f0740323
sadisi <sadisi.rajapaksha218346@gmail.com>
2024-02-12 19:35:17 +0530

    completly developed App UI

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -13,38 +13,86 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: 20, 
-          vertical: 15,
-          ),
-        child: Column(
-          children: [
-           searchBox(),
-           Expanded(
-             child: ListView(
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20, 
+              vertical: 15,
+              ),
+            child: Column(
               children: [
-                Container(
-                  margin: EdgeInsets.only(
-                    top: 50, 
-                    bottom: 20,
+               searchBox(),
+               Expanded(
+                 child: ListView(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(
+                        top: 50, 
+                        bottom: 20,
+                        ),
+
+                      child: Text(
+                        'My ToDos',
+                        style: TextStyle(
+                          fontSize: 30,
+                           fontWeight: FontWeight.w500,
+                           ),
+                         ),
                     ),
-                  child: Text(
-                    'My ToDos',
-                    style: TextStyle(
-                      fontSize: 30,
-                       fontWeight: FontWeight.w500,
-                       ),
-                     ),
-                ),
-                for(ToDo toDo in todosList)
-                ToDoItem(toDo: toDo,),
-            
+                    for(ToDo toDo in todosList)
+                    ToDoItem(toDo: toDo,),
+                
+                  ],
+                 ),
+               )
               ],
-             ),
-           )
-          ],
-        ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                    left: 20,
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 20,vertical: 5),
+                    decoration:  BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0.0,0.0),
+                      blurRadius: 10.0,
+                      spreadRadius: 0.0,
+                      ),],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Add new ToDo Item',
+                        border: InputBorder.none
+                      ),
+                    ),
+                    ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 20,right: 20),
+                      child: ElevatedButton(
+                        child: Text('+',style: TextStyle(fontSize: 40),),
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          primary: tdBlue,
+                          minimumSize: Size(60, 69),
+                          elevation: 10,
+                        ),
+                      ),
+                    )
+            ],),
+          )
+        ],
       ),
     );
   }
