bdfc1a52ff4493c3b3ae645eeb21a44e8090fe7d
sadisi <sadisi.rajapaksha218346@gmail.com>
2024-02-12 17:00:23 +0530

    todoList home page desined nad developed

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/constants/colors.dart';
+import 'package:todo_app/widgets/todo_item.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,10 +11,50 @@ class Home extends StatelessWidget {
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 15),
+        padding: EdgeInsets.symmetric(
+          horizontal: 20, 
+          vertical: 15,
+          ),
         child: Column(
           children: [
-            Container(
+           searchBox(),
+           Expanded(
+             child: ListView(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 50, 
+                    bottom: 20,
+                    ),
+                  child: Text(
+                    'My ToDos',
+                    style: TextStyle(
+                      fontSize: 30,
+                       fontWeight: FontWeight.w500,
+                       ),
+                     ),
+                ),
+                ToDoItem(),
+                ToDoItem(),
+                ToDoItem(),
+                ToDoItem(),
+                ToDoItem(),
+                ToDoItem(),
+                ToDoItem(),
+                ToDoItem(),
+                ToDoItem(),
+                ToDoItem(),
+                ToDoItem(),
+              ],
+             ),
+           )
+          ],
+        ),
+      ),
+    );
+  }
+Widget searchBox(){
+  return  Container(
               padding: EdgeInsets.symmetric(horizontal: 15),
               decoration: BoxDecoration(
                 color: Colors.white,
@@ -36,12 +77,9 @@ class Home extends StatelessWidget {
                       hintStyle: TextStyle(color:tdGrey),
                 ),
               ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
+            );
+
+}
 
   AppBar _buildAppBar() {
     return AppBar(
